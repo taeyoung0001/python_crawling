b0a55ec52e16506d02613f5034f13ed13463dfb8
taeyoung0001 <her020212@naver.com>
2024-12-05 15:52:23 +0900

    [gpt] 오류 발생

---
+++ 05.chatGPT  원고/app.py
@@ -0,0 +1,27 @@
+import openai
+
+api_key = ""
+
+openai.api_key = api_key
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "1+1은 뭐야?"}
+  ],
+  stream=True
+)
+
+result = ""
+
+for chunk in completion:
+  delta_data = chunk.choices[0].delta
+  if 'role' in delta_data:
+    continue
+  elif 'content' in delta_data:
+    r_tex = delta_data['content']
+    result += r_tex
+    print(r_tex, end="", flush=True)
+print("########")
+print(result)
